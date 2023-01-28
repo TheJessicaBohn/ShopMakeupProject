be2f60a97aa5d2d43b0fdebef853488ce4f4ee1a
jessicabohn <jessicabohn74@gmail.com>
2023-01-28 19:48:16 -0300

    Configurando Bootstrap

--- shop_make_up/src/main.ts
+++ shop_make_up/src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(BootstrapVue)
+  .use(IconsPlugin)
+  .mount('#app')
